2739318b61b0475fc5516a15975c934ea517adc8
Charlie <sh.so@octopus-tech.com>
2023-12-21 00:19:48 +0800

    disabled register page

--- src/components/drawer/DrawerItem.jsx
+++ src/components/drawer/DrawerItem.jsx
@@ -1,14 +1,17 @@
+import { Tooltip } from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 
-export const DrawerItem = ({ icon, text, url }) => {
+export const DrawerItem = ({ icon, text, url, disabled }) => {
   return (
-    <ListItem button component={RouterLink} to={url}>
-      <ListItemIcon>{icon}</ListItemIcon>
-      <ListItemText primary={text} />
-    </ListItem>
+
+        <ListItem button component={RouterLink} to={url} disabled={disabled}>
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+
   );
 };
