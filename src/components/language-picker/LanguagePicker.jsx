f6d152a2032ff8ed41f93c0696fb130fcf083dec
Michae <michael.chu.0708@gmail.com>
2023-12-30 22:45:42 +0800

    Update: Mui setup

--- src/components/language-picker/LanguagePicker.jsx
+++ src/components/language-picker/LanguagePicker.jsx
@@ -34,27 +34,8 @@ const LANGUAGE = [
 ]
 export const LanguageSelector = () => {
     const {i18n} = useTranslation();
-    const [isOpen, setIsOpen] = useState(true);
     const selectedLanguage = LANGUAGE.find(language => language.key === i18n.language);
 
-    const handleLanguageChange = async (language) => {
-        await i18n.changeLanguage(language.key);
-        setIsOpen(false);
-    };
-
-    useEffect(() => {
-        const handleWindowClick = (event) => {
-            const target = event.target.closest('button');
-            if (target && target.id === LANGUAGE_SELECTOR_ID) {
-                return;
-            }
-            setIsOpen(false);
-        }
-        window.addEventListener('click', handleWindowClick)
-        return () => {
-            window.removeEventListener('click', handleWindowClick);
-        }
-    }, []);
 
     if (!selectedLanguage) {
         console.log(i18n.language);
@@ -62,85 +43,27 @@ export const LanguageSelector = () => {
     }
 
     return (
-        // <>
-        //     <div className="flex items-center z-40">
-        //         <div className="relative inline-block text-left">
-        //             <div>
-        //                 <button
-        //                     onClick={() => setIsOpen(!isOpen)}
-        //                     type="button"
-        //                     className="inline-flex items-center justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        //                     id={LANGUAGE_SELECTOR_ID}
-        //                     aria-haspopup="true"
-        //                     aria-expanded={isOpen}
-        //                 >
-        //                     <FlagIcon countryCode={selectedLanguage.key.split('-')[0]}/>
-        //                     {selectedLanguage.name}
-        //                     <svg
-        //                         className="-mr-1 ml-2 h-5 w-5"
-        //                         xmlns="http://www.w3.org/2000/svg"
-        //                         viewBox="0 0 20 20"
-        //                         fill="currentColor"
-        //                         aria-hidden="true"
-        //                     >
-        //                         <path
-        //                             fillRule="evenodd"
-        //                             d="M10.293 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 12.586l3.293-3.293a1 1 0 011.414 1.414l-4 4z"
-        //                             clipRule="evenodd"
-        //                         />
-        //                     </svg>
-        //                 </button>
-        //             </div>
-        //             {isOpen && <div
-        //                 className="origin-top-right absolute right-0 mt-2 w-96 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-        //                 role="menu"
-        //                 aria-orientation="vertical"
-        //                 aria-labelledby="language-selector"
-        //             >
-        //                 <div className="py-1 grid grid-cols-2 gap-2" role="none">
-        //                     {LANGUAGE.map((language, index) => {
-        //                         return (
-        //                             <button
-        //                                 key={language.key}
-        //                                 onClick={() => handleLanguageChange(language)}
-        //                                 className={`${
-        //                                     selectedLanguage.key === language.key
-        //                                         ? "bg-gray-100 text-gray-900"
-        //                                         : "text-gray-700"
-        //                                 } block px-4 py-2 text-sm text-left items-center inline-flex hover:bg-gray-100 ${index % 2 === 0 ? 'rounded-r' : 'rounded-l'}`}
-        //                                 role="menuitem"
-        //                             >
-        //                                 <FlagIcon countryCode={language.key.split('-')[0]}/>
-        //                                 <span className="truncate">{language.name}</span>
-        //                             </button>
-        //                         );
-        //                     })}
-        //                 </div>
-        //             </div>}
-        //         </div>
-        //     </div>
-        // </>
         <Box>
-        <Select
-          sx={{ width: 130 }}
-          defaultValue=""
-          displayEmpty
-          renderValue={(value) => {
-            console.log(value);
-            return (
-              <Box sx={{ display: "flex", gap: 1 }}>
-                <FlagIcon countryCode={selectedLanguage.key.split('-')[0]}/>
-                {selectedLanguage.name}
-              </Box>
-            );
-          }}
-        >
-          {LANGUAGE.map((option) => (
-            <MenuItem key={option.key} value={option.key} name={option.name}>
-              {option.name}
-            </MenuItem>
-          ))}
-        </Select>
+            <Select
+                sx={{ width: 130 }}
+                defaultValue={selectedLanguage.key}
+                renderValue={(value) => {
+                    console.log(value);
+                    return (
+                    <Box sx={{ display: "flex", gap: 1 }}>
+                        <FlagIcon countryCode={selectedLanguage.key.split('-')[0]}/>
+                        {selectedLanguage.name}
+                    </Box>
+                    );
+                }}
+            >
+            {LANGUAGE.map((option) => (
+                <MenuItem key={option.key} value={option.key} name={option.name}>
+                <FlagIcon countryCode={option.key.split('-')[0]}/>
+                {option.name}
+                </MenuItem>
+            ))}
+            </Select>
       </Box>
     );
 };
