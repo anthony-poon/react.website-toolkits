cd8b336eddcbe1f7a6c0aff12ed0c8db6b4610bd
Michae <michael.chu.0708@gmail.com>
2023-12-30 23:20:01 +0800

    Update: fr typo

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json'
-import fr from './locales/en.json'
+import fr from './locales/fr.json'
 
 const resources = {
   'en-GB': {
