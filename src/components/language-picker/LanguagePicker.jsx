d19be82a21e616b21291eced55386656bc7277e0
Michae <michael.chu.0708@gmail.com>
2023-12-30 23:06:10 +0800

    Update: multiple languages support

--- src/components/language-picker/LanguagePicker.jsx
+++ src/components/language-picker/LanguagePicker.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
-import {Select, MenuItem, Box} from "@material-ui/core";
+import {Select, MenuItem, Box, makeStyles} from "@material-ui/core";
 import './languagePicker.css';
-
+const useStyle = makeStyles((theme) => ({
+    select:{
+        color: '#FFF !important',
+    },
+    textBox:{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexDirection: 'column',
+        paddingLeft: theme.spacing(1)
+    }
+  }));
 function FlagIcon({countryCode = ""}) {
 
     if (countryCode === "en") {
@@ -31,26 +42,44 @@ const LANGUAGE = [
 ]
 export const LanguageSelector = () => {
     const {i18n} = useTranslation();
+    const classes = useStyle();
     const selectedLanguage = LANGUAGE.find(language => language.key === i18n.language);
     return (
         <Select
+            className={classes.select}
             sx={{ width: 130 }}
-            defaultValue={selectedLanguage.key}
-            onChange={(value) => i18n.changeLanguage(value)}
+            defaultValue={'en-GB'}
+            disableUnderline
+            onChange={(e) => i18n.changeLanguage(e.target.value)}
             renderValue={(value) => {
                 console.log(value);
                 return (
                 <Box sx={{ display: "flex", gap: 1 }}>
                     <FlagIcon countryCode={selectedLanguage.key.split('-')[0]}/>
-                    {selectedLanguage.name}
+                    <div className={classes.textBox}>
+                        {selectedLanguage.name}
+                    </div>
                 </Box>
                 );
             }}
+            MenuProps={{
+                anchorOrigin: {
+                  vertical: "bottom",
+                  horizontal: "left"
+                },
+                transformOrigin: {
+                  vertical: "top",
+                  horizontal: "left"
+                },
+                getContentAnchorEl: null
+              }}
         >
         {LANGUAGE.map((option) => (
             <MenuItem key={option.key} value={option.key} name={option.name}>
-            <   FlagIcon countryCode={option.key.split('-')[0]}/>
-                {option.name}
+                <FlagIcon countryCode={option.key.split('-')[0]}/>
+                <div className={classes.textBox}>
+                    {option.name}
+                </div>
             </MenuItem>
         ))}
         </Select>
