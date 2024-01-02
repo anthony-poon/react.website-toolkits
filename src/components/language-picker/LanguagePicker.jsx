f3363264181700afa8f0701796ba0cab41696461
Michae <michael.chu.0708@gmail.com>
2024-01-03 01:29:37 +0800

    Update: flag

--- src/components/language-picker/LanguagePicker.jsx
+++ src/components/language-picker/LanguagePicker.jsx
@@ -29,62 +29,77 @@ function FlagIcon({countryCode = ""}) {
 const LANGUAGE = [
     {
         key: 'en-gb',
+        flag: 'gb',
         name: 'English'
     },
     {
         key: 'sa-ar',
+        flag: 'ar',
         name: 'العربية',
     },
     {
         key: 'hk-hk',
+        flag: 'hk',
         name: '繁體',
     },
     {
         key: 'cn-cn',
+        flag: 'cn',
         name: '简体'
     },
     {
         key: 'es-es',
+        flag: 'es',
         name: 'Español'
     },
     {
         key: 'fr-fr',
+        flag: 'fr',
         name: 'Français'
     },
     {
         key: 'el-gr',
+        flag: 'gr',
         name: 'Ελληνικά'
     },
     {
         key: 'id-id',
+        flag: 'id',
         name: 'Bahasa'
     },
     {
         key: 'jp-jp',
+        flag: 'jp',
         name: '日本語'
     },
     {
         key: 'kr-kr',
+        flag: 'kr',
         name: '한국인'
     },
     {
         key: 'pt-pt',
+        flag: 'pt',
         name: 'Português'
     },
     {
         key: 'ru-ru',
+        flag: 'ru',
         name: 'Русский'
     },
     {
         key: 'th-th',
+        flag: 'th',
         name: 'แบบไทย'
     },
     {
         key: 'tr-tr',
+        flag: 'tr',
         name: 'Türkçe'
     },
     {
         key: 'vi-vn',
+        flag: 'vn',
         name: 'Tiếng Việt'
     }
 ]
@@ -92,7 +107,8 @@ export const LanguageSelector = () => {
     const {i18n} = useTranslation();
     const classes = useStyle();
     const selectedLanguage = LANGUAGE.find(language => language.key === i18n.language) || {
-        key: 'en-gb'
+        key: 'en-gb',
+        flag: 'gb',
     };
     return (
         <Select
@@ -105,7 +121,7 @@ export const LanguageSelector = () => {
                 console.log(value);
                 return (
                 <Box sx={{ display: "flex", gap: 1 }}>
-                    <FlagIcon countryCode={selectedLanguage?.key.split('-')[1] || 'gb'}/>
+                    <FlagIcon countryCode={selectedLanguage?.flag || 'gb'}/>
                     <div className={classes.textBox}>
                         {selectedLanguage.name}
                     </div>
@@ -126,7 +142,7 @@ export const LanguageSelector = () => {
         >
         {LANGUAGE.map((option) => (
             <MenuItem key={option.key} value={option.key} name={option.name}>
-                <FlagIcon countryCode={option?.key.split('-')[1]}/>
+                <FlagIcon countryCode={option?.flag}/>
                 <div className={classes.textBox}>
                     {option.name}
                 </div>
