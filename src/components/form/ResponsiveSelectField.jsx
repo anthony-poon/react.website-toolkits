073820faa85a131c772fa037c439ec0d62b974f5
Anthony Poon <ypoon196@gmail.com>
2024-03-03 17:48:39 +0800

    Select Field will now accept options prop, and will render options if no children is provided

--- src/components/form/ResponsiveSelectField.jsx
+++ src/components/form/ResponsiveSelectField.jsx
@@ -1,12 +1,35 @@
-import { Box, FormHelperText, Grid, InputLabel, Select, useMediaQuery, useTheme } from "@mui/material";
+import { Box, FormHelperText, Grid, InputLabel, MenuItem, Select, useMediaQuery, useTheme } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
+import PropTypes from "prop-types";
 import React from "react";
 
 import { DualLineLabel } from "./DualLineLabel";
 import { FormFieldWrapper } from "./FormFieldWrapper";
 
+// Ugly work around because Select expect <MenuItem/> as immediate children, React.Fragment will brake it
+// Render children if it is provided, this is for backward compatibility
+// Should use options
+const renderChildrenOrOptions = (children, options = [], required = false) => {
+  if (children) {
+    return children;
+  }
+  const rtn = [];
+  if (!required) {
+    rtn.push(<MenuItem value={""} key={""} disabled={true} />);
+  }
+  options.forEach(({ display, value, key = null }) =>
+    rtn.push(
+      <MenuItem value={value} key={key || value}>
+        {" "}
+        {display}
+      </MenuItem>,
+    ),
+  );
+  return rtn;
+};
+
 const CompactSelectField = (props) => {
-  const { isError, error, children, label, value, name, onChange, required, ...rest } = props;
+  const { isError, error, children, label, value, name, onChange, required, options, ...rest } = props;
   // TODO: implement required and subLabel
   // TODO: Fix label position when label is empty
   return (
@@ -21,7 +44,7 @@ const CompactSelectField = (props) => {
           onChange={onChange}
           label={label}
           {...rest}>
-          {children}
+          {renderChildrenOrOptions(children, options, required)}
         </Select>
         {error && <FormHelperText>{error}</FormHelperText>}
       </FormControl>
@@ -30,7 +53,7 @@ const CompactSelectField = (props) => {
 };
 
 const ExpandedSelectField = (props) => {
-  const { isError, required, error, children, label, subLabel, value, name, onChange, ...rest } = props;
+  const { isError, required, error, children, label, subLabel, value, name, onChange, options, ...rest } = props;
   return (
     <Grid container>
       <Grid item sm={3}>
@@ -42,7 +65,7 @@ const ExpandedSelectField = (props) => {
         <Box mt={2} mb={1}>
           <FormControl variant={"standard"} error={isError} style={{ width: "100%" }}>
             <Select variant={"standard"} fullWidth value={value ? value : ""} name={name} onChange={onChange} {...rest}>
-              {children}
+              {renderChildrenOrOptions(children, options, required)}
             </Select>
             {error && <FormHelperText>{error}</FormHelperText>}
           </FormControl>
@@ -74,4 +97,16 @@ export const ResponsiveSelectField = (props) => {
       </Box>
     </FormFieldWrapper>
   );
+};
+
+ResponsiveSelectField.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      display: PropTypes.any,
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number.isRequired]),
+      key: PropTypes.string,
+    }),
+  ),
+  onChange: PropTypes.func,
 };
