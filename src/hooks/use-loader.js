52615c1d8652e6e9e34b51a6e02d3b0739fea4bd
Anthony Poon <ypoon196@gmail.com>
2024-07-12 16:58:08 +0200

    Added loader hook

---
+++ src/hooks/use-loader.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const useLoader = (init) => {
+  const [isLoading, setLoading] = useState(init);
+  useEffect(() => {
+    if (isLoading) {
+      document.body.style.cursor = "wait";
+    } else {
+      document.body.style.cursor = "default";
+    }
+    return () => {
+      document.body.style.cursor = "default";
+    };
+  }, [isLoading]);
+  return { isLoading, setLoading };
+};
