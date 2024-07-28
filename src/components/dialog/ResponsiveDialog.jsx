64526fee3ee713e4f437c0176be3e215f604e6cb
Anthony Poon <yan.chi.poon@sap.com>
2024-07-28 11:18:44 +0200

    Small code review adjustment

--- src/components/dialog/ResponsiveDialog.jsx
+++ src/components/dialog/ResponsiveDialog.jsx
@@ -1,31 +1,21 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
-import { SuperAdminEditAnswerApp } from '../../../../../view/s-admin/answers/SuperAdminEditAnswerApp';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import * as React from "react";
 
-export default function ResponsiveDialog({ title, isOpen = false, onClose, onSubmit, children }) {
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-    return (
-        <React.Fragment>
-            <Dialog
-                fullScreen={fullScreen}
-                fullWidth={!fullScreen}
-                maxWidth="md"
-                open={isOpen}
-                onClose={onClose}
-                aria-labelledby="responsive-dialog-title"
-            >
-                <DialogContent>
-                    {children}
-                </DialogContent>
-            </Dialog>
-        </React.Fragment>
-    );
-}
+export const ResponsiveDialog = ({ isOpen = false, onClose, children }) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Dialog
+      fullScreen={fullScreen}
+      fullWidth={!fullScreen}
+      maxWidth="md"
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="responsive-dialog-title">
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
+  );
+};
