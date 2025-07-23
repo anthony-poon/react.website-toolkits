22e4c1f0834860617d2455796a6b254564874c80
Anthony Poon <ypoon196@gmail.com>
2025-07-23 15:15:53 +0200

    Added ref forwarding for async div

--- src/components/div/AsyncDiv.tsx
+++ src/components/div/AsyncDiv.tsx
@@ -1,50 +1,36 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 
 import { SpinnerDiv } from "./SpinnerDiv";
 
 interface AsyncDivProps {
-  onMount?: () => Promise<unknown>;
+  onMount?: () => Promise<unknown> | (() => unknown);
   onError?: (error: unknown) => void;
   children?: React.ReactNode;
   duration?: number;
+  fullscreen?: boolean;
 }
 
-export const AsyncDiv: React.FC<AsyncDivProps> = (props) => {
+type AsyncDivHandle = {
+  reload: () => void;
+};
+
+// eslint-disable-next-line react/display-name
+export const AsyncDiv = forwardRef<AsyncDivHandle, AsyncDivProps>((props, ref) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const mountRef = useRef(false);
+
+  const load = async () => {
+    setIsLoading(true);
+    await props.onMount();
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    mountRef.current = true;
-
-    (async () => {
-      try {
-        await Promise.all([
-          props.onMount?.()  ?? Promise.resolve(),
-          new Promise((resolve) => setTimeout(resolve, props.duration || 1500)),
-        ]);
-        if (mountRef.current) {
-          setIsLoading(false);
-        }
-      } catch (error) {
-        if (mountRef.current) {
-          setIsError(true);
-        }
-        props.onError?.(error);
-      }
-    })();
-
-    return () => {
-      mountRef.current = false;
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    (async () => await load())();
   }, []);
-  return (
-    <SpinnerDiv
-      isLoading={isLoading}
-      isError={isError}
-    >
-      {props.children}
-    </SpinnerDiv>
-  );
-};
+
+  useImperativeHandle(ref, () => ({
+    reload: load,
+  }));
+
+  return <SpinnerDiv isLoading={isLoading}>{props.children}</SpinnerDiv>;
+});
