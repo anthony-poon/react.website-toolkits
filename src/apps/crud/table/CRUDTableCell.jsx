38826b4fce4b5ee51fc4c24e6681ea7d9bc2753d
WanYeung-isto <wan.yeung@isto.ch>
2025-09-11 05:08:17 +0800

    Add Relative Test Date and Hide Past Test for Upcoming Tests

--- src/apps/crud/table/CRUDTableCell.jsx
+++ src/apps/crud/table/CRUDTableCell.jsx
@@ -19,3 +19,17 @@ export const TimeTableCell = ({ value }) => {
   const str = value ? DateTime.fromISO(value, { setZone: true }).toFormat("hh:mm a") : "-";
   return <>{str}</>;
 };
+
+export const RelativeDateTableCell = ({ value }) => {
+  const priorityToRelativeDate = {
+    1: "Today",
+    2: "These 3 Days",
+    3: "This Week",
+    4: "This Month",
+    5: "This Year",
+    6: "More than a Year",
+  };
+
+  const str = priorityToRelativeDate[value] || "-";
+  return <>{str}</>;
+};
