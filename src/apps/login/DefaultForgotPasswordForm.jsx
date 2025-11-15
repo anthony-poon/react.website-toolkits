7f394accb3cca1d2a47fed30b8f935f925929103
Anthony Poon <ypoon196@gmail.com>
2025-11-15 17:54:16 +0100

    Fixed forgot password form

--- src/apps/login/DefaultForgotPasswordForm.jsx
+++ src/apps/login/DefaultForgotPasswordForm.jsx
@@ -6,56 +6,42 @@ import Typography from "@mui/material/Typography";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-import { AsyncButton, CardWithIcon } from "../../components";
+import { AsyncButton, CardWithIcon, ResponsiveTextField } from "../../components";
 import { makeFormData } from "../../hooks";
 
 const useFormData = makeFormData({
   email: "",
 });
 
-export const DefaultForgotPasswordForm = ({ errors, onSubmit, onCancel }) => {
+export const DefaultForgotPasswordForm = ({ onSubmit, onCancel }) => {
   const { t } = useTranslation();
-  const { formData, handleFormChange } = useFormData({
-    email: "",
-  });
+  const { formData, handleFormChange } = useFormData();
   const { email } = formData;
-  const handleCancel = () => onCancel();
-  const handleSubmit = () => onSubmit(email);
   return (
     <CardWithIcon
       icon={<HelpIcon style={{ fontSize: 60 }} color={"primary"} />}
       title={t("recovery.password.recovery")}
       subtitle={t("recovery.password.recover.password.with.email")}>
-      <form onSubmit={handleSubmit}>
-        {errors && (
-          <Typography color={"error"}>
-            <ul>
-              {errors.map((e, i) => (
-                <li key={i}>{e}</li>
-              ))}
-            </ul>
-          </Typography>
-        )}
-        <Box mb={4}>
-          <TextField
-            label={t("email.address")}
-            fullWidth
-            margin="normal"
-            value={email}
+      <form onSubmit={() => onSubmit(email)}>
+        <Box mb={4} mt={5} pt={5}>
+          <ResponsiveTextField
+            compact={true}
+            label="Email"
             name={"email"}
-            type={"email"}
+            value={email}
             onChange={handleFormChange}
+            required
           />
         </Box>
-        <Box mb={2}>
+        <Box mb={2} mx={2}>
           <Grid container spacing={1}>
             <Grid item xs={6}>
-              <Button fullWidth size={"large"} type={"button"} variant={"outlined"} onClick={handleCancel}>
+              <AsyncButton fullWidth size={"large"} type={"button"} variant={"outlined"} onClick={() => onCancel()}>
                 {t("cancel")}
-              </Button>
+              </AsyncButton>
             </Grid>
             <Grid item xs={6}>
-              <AsyncButton fullWidth size={"large"} type={"submit"} onClick={handleSubmit}>
+              <AsyncButton fullWidth size={"large"} type={"submit"} onClick={() => onSubmit(email)}>
                 {t("recover")}
               </AsyncButton>
             </Grid>
