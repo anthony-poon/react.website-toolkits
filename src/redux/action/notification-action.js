c22af74e270bf378c4b3f939f9929381d7f9b42c
Anthony Poon <yan.chi.poon@sap.com>
2025-12-06 22:36:32 +0100

    Added error msg for 401

--- src/redux/action/notification-action.js
+++ src/redux/action/notification-action.js
@@ -29,10 +29,16 @@ export const setSuccessMsg = (message) => {
 
 export const setErrorMsg = (error) => {
   let message;
-  if (typeof message === "string") {
+  if (typeof error === "string") {
     message = error;
-  } else {
+  } else if (error.response?.data?.message) {
     message = error.response?.data?.message || error.message;
+  } else if (error.response?.status === 401) {
+    // I am unable to get spring boot to return 401 with gracefully. There are ways, but feels hacky
+    // instead I am just going to override the error message
+    message = "Authentication failed. Please login again";
+  } else {
+    message = error.message;
   }
   return {
     type: SET_NOTIFICATION,
